526724035bbcd833d1e14554343daf831433aaee
bhaskarpraveen <praveennaidu264@gmail.com>
2020-10-23 17:32:05 +0530

    added date format and time

--- services/date_format.ts
+++ services/date_format.ts
@@ -1,9 +1,11 @@
 let formatDate = function(date:Date,time:String):Date{
 
         let new_date = new Date(date);
-        let new_time = Number(time.charAt(0)+time.charAt(1))
-        new_date.setHours(new_time)
-        return new_date
+        let time_hours = Number(time.charAt(0)+time.charAt(1))
+        new_date.setHours(time_hours);
+        let time_minutes = Number(time.charAt(2)+time.charAt(3))
+        new_date.setMinutes(time_minutes);
+        return new_date;
     
    
 }
