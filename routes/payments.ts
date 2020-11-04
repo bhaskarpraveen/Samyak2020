8098275fc48ce664f472daa42e37b8b8dd8d3a15
bhaskarpraveen <praveennaidu264@gmail.com>
2020-11-04 11:39:18 +0530

    payment request bug fixed

--- routes/payments.ts
+++ routes/payments.ts
@@ -102,7 +102,7 @@ router.post('/add-payment',VerifyToken,async function(request:jwt_request,respon
                     let payment = new Payment({
                         user_id:user._id, 
                         payment_id:payment_response.data['payment_request'].payment.payment_id,
-                        payment_request_id:payment_response.data['payment_request'].payment.payment_request_id,	
+                        payment_request_id:payment_response.data['payment_request'].id,	
                         instrument_type:payment_response.data['payment_request'].payment.instrument_type,
                         billing_instrument:payment_response.data['payment_request'].payment.billing_instrument,
                         status:payment_response.data['payment_request'].payment.status,
