245766cc888dc757c41c229e8ddfc692459bf276
bhaskarpraveen <praveennaidu264@gmail.com>
2020-11-05 19:35:53 +0530

    samyak id patch

--- services/samyak_id_generation.ts
+++ services/samyak_id_generation.ts
@@ -4,11 +4,11 @@ let generateSamyakId = async function(){
     let id_string:String = 'SMK2K';
     let id_num = 20000000 ;
     let total =  await User.find({}).sort('-samyak_id').limit(1);
-    let num:number ;
+    let num;
     if (total.length==0) num=0;
     else{
        num = total[0].samyak_id;
-       num = Number(num.toString().slice(7))+1;
+       num = Number(num.slice(7))+1;
     }
     id_string = id_string+String( id_num + num)
     return id_string
