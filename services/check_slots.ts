e6dccb2feb266d4dda7d36fbc6f557286feb9e7f
bhaskarpraveen <praveennaidu264@gmail.com>
2020-11-09 12:43:33 +0530

    schedule patch fixed

--- services/check_slots.ts
+++ services/check_slots.ts
@@ -14,35 +14,37 @@ let checkSlots =async function(user:any,event:any){
       //slots of the user already registered
         let registered_slots =[];
         for(let i=0;i<registered_events.length;i++){
-            let slots = await EventSlot.find({event_id:event._id});
-            registered_slots.push(slots)
+            let slots = await EventSlot.findOne({event_id:event._id});
+            if(slots) registered_slots.push(slots)
         }
         if(!registered_slots)return true;
         let check=1;
         for(let i=0;i<event_slots.length;i++){
             if(!event_slots[i].multiple_events_allowed){
+
+                let current_slot_start = event_slots[i].date;
+                current_slot_start.setHours(Number(event_slots[i].start_time.split(":")[0]))
+                current_slot_start.setMinutes(Number(event_slots[i].start_time.split(":")[1]))
+                let current_slot_end = event_slots[i].date;
+                current_slot_end.setHours(Number(event_slots[i].end_time.split(":")[0]))
+                current_slot_end.setMinutes(Number(event_slots[i].end_time.split(":")[1]))
+
                 for(let j=0;j<registered_slots.length;j++){
-                    if(!registered_slots[i].multiple_events_allowed){
+                   
+                        if(!registered_slots[j].multiple_events_allowed){
+    
+                            let registered_slot_start = registered_slots[j].date;
+                            registered_slot_start.setHours(Number(registered_slots[j].start_time.split(":")[0]))
+                            registered_slot_start.setMinutes(Number(registered_slots[j].start_time.split(":")[1]))
+                            let registered_slot_end = registered_slots[j].date;
+                            registered_slot_end.setHours(Number(registered_slots[j].end_time.split(":")[0]))
+                            registered_slot_end.setMinutes(Number(registered_slots[j].end_time.split(":")[1]))
 
-
-                        let current_slot_start = event_slots[i].date;
-                        current_slot_start.setHours(Number(event_slots[i].start_time.split(":")[0]))
-                        current_slot_start.setMinutes(Number(event_slots[i].start_time.split(":")[1]))
-                        let current_slot_end = event_slots[i].date;
-                        current_slot_end.setHours(Number(event_slots[i].end_time.split(":")[0]))
-                        current_slot_end.setMinutes(Number(event_slots[i].end_time.split(":")[1]))
-
-                        let registered_slot_start = registered_slots[i].date;
-                        registered_slot_start.setHours(Number(registered_slots[i].start_time.split(":")[0]))
-                        registered_slot_start.setMinutes(Number(registered_slots[i].start_time.split(":")[1]))
-                        let registered_slot_end = registered_slots[i].date;
-                        registered_slot_end.setHours(Number(registered_slots[i].end_time.split(":")[0]))
-                        registered_slot_end.setMinutes(Number(registered_slots[i].end_time.split(":")[1]))
-                      if((current_slot_start>registered_slot_start&&current_slot_start<registered_slot_end)||(current_slot_end>registered_slot_start&&current_slot_end<registered_slot_end)){
-                          check=0;
-                          return false
-                      }
-
+                          if((current_slot_start>registered_slot_start&&current_slot_start<registered_slot_end)||(current_slot_end>registered_slot_start&&current_slot_end<registered_slot_end)){
+                              check=0;
+                              return false
+                          }
+                    
 
                     }
                 }
