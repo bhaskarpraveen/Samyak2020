9c93a780a19a6bb01d2560a6ab109a994b20c1e3
bhaskarpraveen <praveennaidu264@gmail.com>
2020-11-17 23:03:01 +0530

    debugging

--- services/check_slots.ts
+++ services/check_slots.ts
@@ -5,11 +5,13 @@ import EventSlot from '../models/event_slots';
 import UserEventRegistration from '../models/user_event_registrations';
 let checkSlots =async function(user:any,event:any){
     //all registerd events of the user
-    let registered_events = await UserEventRegistration.find({user_id:user._id})
+    let registered_events = await UserEventRegistration.find({user_id:user._id}) 
+    console.log('registerd events:'+JSON.stringify(registered_events))
     if(!registered_events)return true;
     else{
         //slots of the event user currently trying to register
       let event_slots = await EventSlot.find({event_id:event._id})
+      console.log('event slots:'+JSON.stringify(event_slots))
       if(!event_slots) return true;
       //slots of the user already registered
         let registered_slots =[];
@@ -17,6 +19,8 @@ let checkSlots =async function(user:any,event:any){
             let slots = await EventSlot.findOne({event_id:event._id});
             if(slots) registered_slots.push(slots)
         }
+
+        console.log('registered_slots:'+JSON.stringify(registered_slots))
         if(!registered_slots)return true;
         let check=1;
         for(let i=0;i<event_slots.length;i++){
@@ -28,7 +32,8 @@ let checkSlots =async function(user:any,event:any){
                 let current_slot_end = event_slots[i].date;
                 current_slot_end.setHours(Number(event_slots[i].end_time.split(":")[0]))
                 current_slot_end.setMinutes(Number(event_slots[i].end_time.split(":")[1]))
-
+                console.log('current_slot_start:'+current_slot_start)
+                console.log('current_slot_end:'+current_slot_end)
                 for(let j=0;j<registered_slots.length;j++){
                    
                         if(!registered_slots[j].multiple_events_allowed){
@@ -40,7 +45,8 @@ let checkSlots =async function(user:any,event:any){
                             registered_slot_end.setHours(Number(registered_slots[j].end_time.split(":")[0]))
                             registered_slot_end.setMinutes(Number(registered_slots[j].end_time.split(":")[1]))
 
-                          if((current_slot_start>registered_slot_start&&current_slot_start<registered_slot_end)||(current_slot_end>registered_slot_start&&current_slot_end<registered_slot_end)){
+                          if((current_slot_start>=registered_slot_start&&current_slot_start<registered_slot_end)||(current_slot_end>registered_slot_start&&current_slot_end<registered_slot_end)){
+                              console.log('false returned')
                               check=0;
                               return false
                           }
@@ -50,6 +56,7 @@ let checkSlots =async function(user:any,event:any){
                 }
             }
         }
+        console.log('Returnd check:'+check)
         return check;
 
     }
