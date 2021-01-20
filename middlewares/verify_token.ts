065fd9b89f30c405d7a38c4606ac37f8ceb74a27
bhaskarpraveen <praveennaidu264@gmail.com>
2021-01-20 19:23:54 +0530

    token check

--- middlewares/verify_token.ts
+++ middlewares/verify_token.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config()
 let JWT_KEY = process.env.JWT_KEY || 'jsonwebtoken';
 
 interface jwt_request extends express.Request{
@@ -8,7 +10,7 @@ interface jwt_request extends express.Request{
 
 let VerifyToken = async function(request:jwt_request,response:express.Response,next:express.NextFunction){
     const token:string = String(request.headers['x-access-token']);
-    console.log({JWT_KEY})
+    
     jwt.verify(token,JWT_KEY,function(err,data){
         if(err){
             console.log(err)
--- middlewares/verify_user_role.ts
+++ middlewares/verify_user_role.ts
@@ -55,13 +55,13 @@ let VerifyUserRole = (data:role_data)=>{
                     if(allRoles[0].permissions[0].permissions[data.collection][data.permission]){
                         next();
                     }else{
-                        return response.status(500).json({message:'Authorization failed .'});
+                        return response.status(500).json({message:'Authorization failed'});
                     }
                 }else{
-                    return response.status(501).json({message:'Authorization failed No role'})
+                    return response.status(501).json({message:'Authorization failed'})
                 }
             }else{
-                return response.status(501).json({message:'Authorization failed No user'})
+                return response.status(501).json({message:'Authorization failed'})
             }
             }
         }
